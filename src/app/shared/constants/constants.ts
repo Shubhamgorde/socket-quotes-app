2e795e9be613e6046207df3646f4b9a6f91d4438
Shubham Gorde <Shubham.Gorde@darkmatter.ae>
2019-10-17 11:49:06 +0530

    Inital commit added

---
+++ src/app/shared/constants/constants.ts
@@ -0,0 +1,7 @@
+export class Constants {
+  public static readonly NO_CHANGE_COLOR = '#808080';
+  public static readonly RISE_COLOR = '#008000';
+  public static readonly FALL_COLOR = '#FF0000';
+  public static readonly DATE_FORMAT = 'MM/DD/YYYY HH:mm:ss';
+  public static readonly DECIMAL_PLACES = 2;
+}
